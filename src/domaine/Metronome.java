04f3a909dc7d537b6c0bcd7841ea3e2466a363f2
Mathias Boudreau <32655672+mathouqc@users.noreply.github.com>
2025-10-14 21:13:56 -0400

    Fix step 1

--- src/domaine/Metronome.java
+++ src/domaine/Metronome.java
@@ -27,13 +27,13 @@ public class Metronome {
 
     public Metronome() {
         bpm = 80;
-        estEnCours = true;
+        estEnCours = false;
         timer = new Timer((60000/bpm), (ActionEvent e) -> {
                 mChannels[10].noteOn(60, 50);
                 mChannels[10].noteOff(60);
             });
             timer.setRepeats(true);
-            timer.start();
+            // timer.start();
         try {
 
             midiSynth = MidiSystem.getSynthesizer();
